e73f3df457012ca3fc8b70924ea1bc15cc216e3d
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-07-16 17:39:31 +0530

    Added documentation for ColliderBodies.h

--- include/physics/ColliderBodies.h
+++ include/physics/ColliderBodies.h
@@ -7,26 +7,55 @@
 #include <unordered_set>
 #include "Collider.h"
 
-struct ColliderHash {
-    std::size_t operator()(const Collider& collider) const {
+ /**
+ * Hash function for \code std::reference_wrapper<Collider>\endcode
+ */
+struct ColliderRefHash {
+    std::size_t operator()(const std::reference_wrapper<Collider>& ref) const {
+        const Collider& collider = ref.get();
         return std::hash<float>{}(collider.getPosition().x) ^
                std::hash<float>{}(collider.getPosition().y) ^
                std::hash<unsigned long>{}(collider.getHitbox().size());
     }
 };
 
+/**
+ * Equality function for \code std::reference_wrapper<Collider>\endcode
+ */
+struct ColliderRefEqual {
+    bool operator()(const std::reference_wrapper<Collider>& lhs, const std::reference_wrapper<Collider>& rhs) const {
+        return lhs.get() == rhs.get();
+    }
+};
+
 class ColliderBodies {
 public:
+    /**
+     * @brief gives the instance of ColliderBodies.
+     * @return the singleton instance of ColliderBodies.
+     */
     static ColliderBodies& getInstance();
 
-    [[nodiscard]] const std::unordered_set<Collider, ColliderHash>& getBodies() const;
+    /**
+     * @brief A const function that gives all the registered collider bodies.
+     * @return the \code std::unordered_set\endcode that contains the reference to all the bodies.
+     */
+    [[nodiscard]] const std::unordered_set<std::reference_wrapper<Collider>, ColliderRefHash, ColliderRefEqual>& getBodies() const;
 
-    void addBody(Collider body);
+    /**
+     * @brief Registers the body into the collider bodies unordered set.
+     * @param body the body to be registered into the unordered set.
+     */
+    void addBody(Collider& body);
 
-    void removeBody(Collider body);
+    /**
+     * @brief Unregisters the given body from the collider bodies unordered set.
+     * @param body the body to be unregistered from the unordered set.
+     */
+    void removeBody(Collider& body);
 
 private:
-    std::unordered_set<Collider, ColliderHash> bodies;
+    std::unordered_set<std::reference_wrapper<Collider>, ColliderRefHash, ColliderRefEqual> bodies;
 };
 
 #endif //COLLIDERBODIES_H
