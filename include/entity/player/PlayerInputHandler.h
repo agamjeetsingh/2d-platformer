b5282a56c25311e6f7194aa163cc875d3c56f986
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-08-02 18:33:59 +0530

    Implement skeleton of player input handling

---
+++ include/entity/player/PlayerInputHandler.h
@@ -0,0 +1,81 @@
+//
+// Created by Agamjeet Singh on 26/07/25.
+//
+
+#ifndef PLAYERINPUTHANDLER_H
+#define PLAYERINPUTHANDLER_H
+
+#include "Player.h"
+#include "../../utility/InputManager.h"
+#include <SFML/Graphics.hpp>
+
+using namespace sf::Keyboard;
+
+class PlayerInputHandler {
+public:
+    explicit PlayerInputHandler(Player& player): player(player) {}
+
+    void update() {
+        if (isPressed(dashKey)) {
+            tryDash();
+        }
+
+        if (isPressed(moveLeft) && (!isPressed(moveRight) || wasPressedEarlierThan(moveRight, moveLeft))) {
+            // Move left
+            player.facing = Facing::Left;
+            player.velocity.x = -Player::WALK_SPEED;
+        }
+
+        if (isPressed(moveRight) && (!isPressed(moveLeft) || wasPressedEarlierThan(moveLeft, moveRight))) {
+            // Move right
+            player.facing = Facing::Right;
+            player.velocity.x = Player::WALK_SPEED;
+        }
+
+        if (isPressed(jumpKey)) {
+            tryJump();
+        }
+
+        if (isPressed(crouchKey)) {
+            // TODO - Crouch
+        }
+    }
+
+private:
+    Player& player;
+
+    static bool isPressed(const Key key) {
+        return InputManager::getInstance().isPressed(key);
+    }
+
+    static bool wasPressedEarlierThan(const Key key1, const Key key2) {
+        return InputManager::getInstance().wasPressedEarlierThan(key1, key2);
+    }
+
+    Key dashKey = Key::L;
+    Key climbKey = Key::K;
+    Key moveLeft = Key::A;
+    Key moveRight = Key::D;
+    Key moveDown = Key::S;
+    Key moveUp = Key::W;
+    Key crouchKey = Key::S;
+    Key jumpKey = Key::Space;
+
+    void tryDash() {
+        if (!player.canDash()) {
+            return;
+        }
+        // TODO - Change hair color to indicate cannot dash
+    }
+
+    void tryJump() {
+        if (!player.canJump()) {
+            return;
+        }
+        // TODO
+    }
+};
+
+
+
+#endif //PLAYERINPUTHANDLER_H
