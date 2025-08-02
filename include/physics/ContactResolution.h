113e52afaa3e12e6e4fe588cf63d969dd436f238
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-08-02 18:31:46 +0530

    Implemented basic contact resolution

---
+++ include/physics/ContactResolution.h
@@ -0,0 +1,17 @@
+//
+// Created by Agamjeet Singh on 24/07/25.
+//
+
+#ifndef CONTACTRESOLUTION_H
+#define CONTACTRESOLUTION_H
+
+#include "../events/Collision.h"
+
+class ContactResolution {
+public:
+    static void resolve(Collision collision);
+};
+
+
+
+#endif //CONTACTRESOLUTION_H
---
+++ src/physics/ContactResolution.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Agamjeet Singh on 24/07/25.
+//
+
+#include "../../include/physics/ContactResolution.h"
+
+void ContactResolution::resolve(Collision collision) {
+    // TODO - improve later if needed
+
+    CollidableObject& object = (collision.objectA.type == CollidableObjectType::Movable) ? collision.objectA : collision.objectB;
+
+    if (collision.axis == CollisionAxis::Down || collision.axis == CollisionAxis::Up) {
+        object.velocity.y = 0;
+    } else {
+
+        object.velocity.x = 0;
+    }
+}
