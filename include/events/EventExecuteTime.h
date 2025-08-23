ecfd57b003928514a4cd90b6b986e68d46c9e4a4
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-08-23 11:33:23 +0530

    Implemented EventExecuteTime.h enum

---
+++ include/events/EventExecuteTime.h
@@ -0,0 +1,18 @@
+//
+// Created by Agamjeet Singh on 17/08/25.
+//
+
+#ifndef EVENTEXECUTETIME_H
+#define EVENTEXECUTETIME_H
+
+
+
+enum class EventExecuteTime {
+    NOW,
+    PRE_PHYSICS,
+    POST_PHYSICS
+};
+
+
+
+#endif //EVENTEXECUTETIME_H
