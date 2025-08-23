e15b5a280bc04c3656f67ecb3c11d62cfc0ab92a
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-08-23 11:33:05 +0530

    Brought template functions of Event to header file

--- include/events/Event.h
+++ include/events/Event.h
@@ -14,16 +14,22 @@ class DummyEvent {};
 class Event {
 public:
     template <typename T>
-    explicit Event(T event, EventExecuteTime execute_time);
+    explicit Event(T event, EventExecuteTime execute_time) : type_index(typeid(event)), execute_time(execute_time), meta_data(std::make_shared<GeneralEvent<T>>(std::move(event))) {}
 
     const std::type_index type_index;
     const EventExecuteTime execute_time;
 
     template <typename EventType>
-    [[nodiscard]] const EventType* getIf() const;
+    [[nodiscard]] const EventType* getIf() const {
+        if (type_index != typeid(EventType)) return nullptr;
+        auto ptr = dynamic_cast<GeneralEvent<EventType>*>(&meta_data);
+        return ptr ? &ptr->data : nullptr;
+    }
 
     template <typename EventType>
-    [[nodiscard]] bool is() const;
+    [[nodiscard]] bool is() const {
+        return type_index == typeid(EventType);
+    }
 
 private:
     struct GeneralEventBase {
--- src/events/Event.cpp
+++ src/events/Event.cpp
@@ -4,18 +4,4 @@
 
 #include "../../include/events/Event.h"
 
-template<typename T>
-Event::Event(T event, EventExecuteTime execute_time): type_index(typeid(event)), execute_time(execute_time), meta_data(std::make_shared<GeneralEvent<T>>(std::move(event))) {}
-
-template<typename EventType>
-const EventType *Event::getIf() const {
-    if (type_index != typeid(EventType)) return nullptr;
-    auto ptr = dynamic_cast<GeneralEvent<EventType>*>(&meta_data);
-    return ptr ? &ptr->data : nullptr;
-}
-
-template<typename EventType>
-bool Event::is() const {
-    return type_index == typeid(EventType);
-}
 
