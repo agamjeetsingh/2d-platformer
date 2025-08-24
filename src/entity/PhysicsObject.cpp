0f72da0c76434478e3f87c25516b7bc3ff3aeacc
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-08-24 18:41:21 +0530

    Wrote documentation for PhysicsObject and removed unused update function

--- src/entity/PhysicsObject.cpp
+++ src/entity/PhysicsObject.cpp
@@ -10,8 +10,20 @@ PhysicsObject::PhysicsObject(sf::Sprite sprite, sf::Vector2f position = {0, 0}) 
     this->sprite.setPosition(position);
 }
 
-void PhysicsObject::update(float deltaTime) {
-    sprite.setPosition(position);
+const sf::Vector2f &PhysicsObject::getPositionRef() const {
+    return position;
+}
+
+void PhysicsObject::setGravityVelocity(sf::Vector2f velocity) {
+    assert(velocity.x == 0);
+    gravity_velocity = velocity;
+    gravity_velocity.y = std::min(gravity_velocity.y, MAX_FALL);
+}
+
+void PhysicsObject::addGravityVelocity(sf::Vector2f velocity) {
+    assert(velocity.x == 0);
+    gravity_velocity += velocity;
+    gravity_velocity.y = std::min(gravity_velocity.y, MAX_FALL);
 }
 
 const sf::Sprite& PhysicsObject::getSprite() const {
@@ -33,7 +45,22 @@ void PhysicsObject::addPosition(sf::Vector2f position) {
 }
 
 sf::Vector2f PhysicsObject::getTotalVelocity() const {
-    return intrinsic_velocity + friction_velocity + impulse_velocity;
+    return intrinsic_velocity + friction_velocity + impulse_velocity + gravity_velocity;
 }
 
+void PhysicsObject::printVelocity(const std::string &name) const {
+    std::cout << name << " intrinsic_velocity: x = " << intrinsic_velocity.x << ", y = " << intrinsic_velocity.y << std::endl;
+    std::cout << name << " friction_velocity: x = " << friction_velocity.x << ", y = " << friction_velocity.y << std::endl;
+    std::cout << name << " impulse_velocity: x = " << impulse_velocity.x << ", y = " << impulse_velocity.y << std::endl;
+}
+
+void PhysicsObject::printVelocity() const {
+    printVelocity(getAddressAsString());
+}
+
+std::string PhysicsObject::getAddressAsString() const {
+    std::ostringstream oss;
+    oss << this;
+    return oss.str();
+}
 
