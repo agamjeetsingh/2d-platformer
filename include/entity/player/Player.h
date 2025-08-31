602b3f0a04e7416089accf7871cd0c5458d92974
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-08-31 18:48:44 +0530

    Updated player class to now have listeners and use new ability system to manage state

--- include/entity/player/Player.h
+++ include/entity/player/Player.h
@@ -7,16 +7,21 @@
 
 #include <SFML/Graphics.hpp>
 
+#include "AbilityDash.h"
+#include "AbilityJump.h"
 #include "DashDirection.h"
 #include "Facing.h"
 #include "PlayerSpriteState.h"
 #include "PlayerState.h"
-#include "PlayerStateGround.h"
 #include "../CollidableObject.h"
 #include "sprites/PlayerSpriteHandler.h"
 #include "../../utility/Scheduler.h"
+#include "events/Listener.h"
+#include "events/PlayerLeftGround.h"
+#include "physics/ContactsHandler.h"
 
 class ContactsHandler;
+struct PlayerOnGround;
 
 class Player final : public CollidableObject {
 public:
@@ -26,13 +31,15 @@ public:
 
     static constexpr float MAX_STAMINA = 110;
     static constexpr float JUMP_GRACE_COYOTE_TIME = 0.1; // In seconds
-    static constexpr float JUMP_GRACE_LANDING_TIME = 0.2; // In seconds
+    static constexpr float JUMP_GRACE_BUFFER_TIME = 0.2; // In seconds
+    static constexpr float VARIABLE_JUMP_TIME = 0.2; // In seconds
     static constexpr float WALK_SPEED = 90;
     static constexpr float RUN_REDUCE = 400;
     static constexpr float RUN_ACCELERATION = 1000;
     static constexpr float AIR_MULTIPLIER = 0.65;
     static constexpr float JUMP_SPEED = -105;
-    static constexpr float JUMP_H_BOOST = 40;
+    static constexpr float JUMP_HORIZONTAL_BOOST = 40;
+    static constexpr float HALF_GRAVITY_THRESHOLD = 40;
     static constexpr float UPWARD_CORNER_CORRECTION = 4;
 
     static constexpr float DASH_SPEED = 240;
@@ -40,107 +47,100 @@ public:
     static constexpr float DASH_FREEZE_TIME = 0.05; // In seconds
     static constexpr float DASH_SPEED_TIME = 0.2; // In seconds
     static constexpr float TOTAL_DASH_TIME = DASH_FREEZE_TIME + DASH_SPEED_TIME;
+    static constexpr float DASH_COOLDOWN_TIME = TOTAL_DASH_TIME; // In seconds, includes freeze time and dashing
     static constexpr float DASH_RESET_SPEED_HORIZONTAL = 160;
     static constexpr float DASH_RESET_SPEED_UP_DIAGONAL = 160;
     static constexpr float DASH_RESET_SPEED_VERTICAL = 120;
     static constexpr DashDirection DEFAULT_DASH_DIRECTION = DashDirection::RIGHT;
+    static constexpr float DASH_FIRST_SNAPSHOT_TIME = 0.08;
+    static constexpr float DASH_SECOND_SNAPSHOT_TIME = 0.15;
+    static constexpr float DASH_THIRD_SNAPSHOT_TIME = 0.2;
     // Dash reset speed for down diagonal is the current speed (it is retained)
 
-    bool dash_reset_timer_over = true;
+    static constexpr float GRAVITY = 900;
+
     float stamina = MAX_STAMINA;
     PlayerSpriteState sprite_state = PlayerSpriteState::Ground;
     Facing facing = Facing::Right;
 
-    void updateSprite(float deltaTime);
+    void updateSprite(float deltaTime) {
+        sprite_handler.update(deltaTime);
+    }
 
-    // ===== State Methods ====
-    void changeStateTo(PlayerState& new_state);
-    void runDuringState();
-    PlayerState& getState() const;
-    void updateState();
+    // ===== Gravity Movement Methods ====
+    void enableGravity() {
+        gravity_acceleration.y = GRAVITY;
+    }
 
-    bool isOnGround() const {
+    void disableGravity() {
+        gravity_acceleration.y = 0;
+    }
+
+    // ===== State Retrieval Methods =====
+
+    [[nodiscard]] bool isOnGround() const {
         return onGround;
     }
 
-    /**
-     * @brief Restores dash capacity and stamina. Must be called each frame the player is on ground.
-     */
-    void onLand();
+    [[nodiscard]] bool canJump() const {
+        return ability_jump.canPerform();
+    }
 
-    /**
-     * Sets \code canJumpDueToGrace\endcode and schedules an event to set it to false after \code JUMP_GRACE_TIME\endcode
-     * seconds. Must be called only at the frame the player leaves the ground. Should not be called every time the
-     * player is in the air.
-     */
-    void noLongerOnGround();
-
-    [[nodiscard]] bool canJump() const;
-
-    [[nodiscard]] bool canWallJump() const;
-
-    /**
-     * Sets \code canJumpDueToGrace\endcode flag to false. Must only be called at the frame the player jumps.
-     */
-    void justJumped();
+    [[nodiscard]] bool canWallJump() const {
+        return isClimbing().has_value(); // TODO
+    }
 
     [[nodiscard]] bool canDash() const {
-        return dashCapacity;
+        return ability_dash.canPerform();
     }
 
     [[nodiscard]] std::optional<std::reference_wrapper<CollidableObject>> isClimbing() const {
-        return climbing;
+        return climbing; // TODO
     }
 
-    void tryJumpInFuture(float seconds) {
-        std::function<void()> cb = [this]{ tryJump(); };
-        ScheduledEvent event = {cb, 0, true, 0, 0.2};
-        Scheduler::getInstance().schedule(event);
-    }
+    // ===== Try to Jump, Dash, Climb Methods =====
 
-    bool tryJump() {
-        if (canJump()) {
-            return false;
-        }
-        // TODO
+    // Jump buffering
+    void tryJumpInFuture();
 
-        return true;
-    }
+    bool tryJump();
 
-    bool tryDash(DashDirection direction) {
-        if (!canDash()) {
-            return false;
-        }
-        // TODO - Change hair color to indicate cannot dash
-
-        // TODO - Dash Freeze
-
-        auto vector = directionToVector(direction);
-        auto dash_velocity = vector * DASH_SPEED;
-        intrinsic_velocity = (intrinsic_velocity.length() > dash_velocity.length()) ? intrinsic_velocity : dash_velocity;
-
-        return true;
-    }
+    bool tryDash();
 
     bool tryClimb() {
         return false; // TODO
     }
 
+    // ===== Abilities ====
+
+    // Jump
+    bool canJumpDueToCoyoteGrace = false;
+    AbilityJump ability_jump{*this};
+
+    // Dash
+    bool dashCapacity = true;
+    AbilityDash ability_dash{*this};
+
 private:
     PlayerSpriteHandler sprite_handler = PlayerSpriteHandler(sprite_state, sprite, facing);
 
-    PlayerState* state = &PlayerStateGround::getInstance();
+    Listener on_ground;
 
-    bool onGround = true;
+    Listener left_ground;
 
-    bool dashCapacity = true;
-    bool canJumpDueToGrace = false;
+    Listener landed;
+
+    bool onGround = false;
 
     // Also need to store information about which direction climbing which I guess is stored in facing
     std::optional<std::reference_wrapper<CollidableObject>> climbing = std::nullopt;
 
-    void restoreStamina();
-    void restoreDash();
+    void restoreStamina() {
+        stamina = MAX_STAMINA;
+    }
+    void restoreDash() {
+        dashCapacity = true;
+    }
 };
 
 #endif //PLAYER_H
