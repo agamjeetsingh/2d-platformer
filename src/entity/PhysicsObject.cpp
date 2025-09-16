1aecf860badbc4f864c8bf396c183b1298e11841
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-09-16 12:28:19 +0530

    Shifted definition of squeeze method from header to .cpp file in PhysicsObject.cpp

--- src/entity/PhysicsObject.cpp
+++ src/entity/PhysicsObject.cpp
@@ -51,3 +51,40 @@ std::string PhysicsObject::getAddressAsString() const {
     return oss.str();
 }
 
+void PhysicsObject::squeeze(sf::Vector2f target_squeeze, float time_to_squeeze, float time_to_unsqueeze) {
+    auto discard1 = Scheduler::getInstance().schedule([total_time = 0.f, time_to_squeeze, target_squeeze, this, time_to_unsqueeze](const std::shared_ptr<ScheduledEvent>& event, float dt) mutable {
+        total_time += dt;
+        float t = std::min(total_time,time_to_squeeze);
+        float alpha = t / time_to_squeeze;
+        alpha = alpha * alpha * (3 - 2 * alpha);
+        sf::Vector2f startScale = {1, 1};
+        sf::Vector2f targetScale = target_squeeze;
+        sf::Vector2f current = startScale * (1 - alpha) + targetScale * alpha;
+        current.x *= sprite.getScale().x > 0 ? 1 : -1;
+        current.y *= sprite.getScale().y > 0 ? 1 : -1;
+        sprite.setScale(current);
+
+        if (t == time_to_squeeze) {
+            event->cancel();
+            // Schedule next one
+            auto discard2 = Scheduler::getInstance().schedule([total_time = 0.f, target_squeeze, time_to_unsqueeze, this](const std::shared_ptr<ScheduledEvent>& event, float dt) mutable {
+                total_time += dt;
+                float t = std::min(total_time,time_to_unsqueeze);
+                float alpha = t / time_to_unsqueeze;
+                alpha = alpha * alpha * (3 - 2 * alpha);
+                sf::Vector2f startScale = target_squeeze;;
+                sf::Vector2f targetScale = {1, 1};
+                sf::Vector2f current = startScale * (1 - alpha) + targetScale * alpha;
+                current.x *= sprite.getScale().x > 0 ? 1 : -1;
+                current.y *= sprite.getScale().y > 0 ? 1 : -1;
+                sprite.setScale(current);
+
+                if (t == time_to_unsqueeze) {
+                    event->cancel();
+                }
+            }, 0, true, 0);
+        }
+    }, 0, true, 0);
+}
+
+
