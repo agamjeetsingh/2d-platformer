479c6f92a882eaafc1e217038cf711ba65d80974
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-09-21 17:53:47 +0530

    Implemented PhysicsObject in 3D

---
+++ include/physics/3d/PhysicsObject.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Agamjeet Singh on 21/09/25.
+//
+
+#include "PhysicsObject.h"
+
+#include <iostream>
+
+eng::d3::PhysicsObject::PhysicsObject(sf::Vector3f position = {0, 0, 0}) : position(position) {}
+
+sf::Vector3f eng::d3::PhysicsObject::getTotalVelocity() const {
+    return base_velocity + friction_velocity;
+}
+
+void eng::d3::PhysicsObject::printVelocity(const std::string &name) const {
+    std::cout << name << " base_velocity: x = " << base_velocity.x << ", y = " << base_velocity.y << std::endl;
+    std::cout << name << " friction_velocity: x = " << friction_velocity.x << ", y = " << friction_velocity.y << std::endl;
+}
+
+void eng::d3::PhysicsObject::printVelocity() const {
+    printVelocity(getAddressAsString());
+}
+
+std::string eng::d3::PhysicsObject::getAddressAsString() const {
+    std::ostringstream oss;
+    oss << this;
+    return oss.str();
+}
---
+++ include/physics/3d/PhysicsObject.h
@@ -0,0 +1,81 @@
+//
+// Created by Agamjeet Singh on 21/09/25.
+//
+
+#ifndef PHYSICSOBJECT_H
+#define PHYSICSOBJECT_H
+
+#include <SFML/Graphics.hpp>
+
+namespace eng::d3 {
+    class PhysicsObject {
+    public:
+        /**
+         * @brief Constructs the Entity with the given sprite and position. Sets the sprite to the given position.
+         * @param position the entity's position upon construction.
+         */
+        explicit PhysicsObject(sf::Vector3f position);
+
+        virtual ~PhysicsObject() = default;
+
+        /**
+         * @brief Maximum velocity of the gravity. This maximum is enforced by \code setGravityVelocity()\endcode and
+         * \code addGravityVelocity()\endcode.
+         */
+        static constexpr float MAX_FALL = 240;
+
+        /**
+        * @brief The PhysicsObject's position.
+        */
+        sf::Vector3f position;
+
+        /**
+         * @brief The base velocity of the object. It will always try to move with the base velocity.
+         */
+        sf::Vector3f base_velocity = {0, 0, 0};
+
+        /**
+         * @brief The velocity imparted by the surface the object is resting on (if any) or if the object is a player
+         * climbing another object.
+         */
+        sf::Vector3f friction_velocity = {0, 0, 0};
+
+        /**
+         * @brief Returns the total velocity of the PhysicsObject. This is the object's true overall velocity.
+         * @return The total velocity - sum of intrinsic, friction, impulse and gravitational velocities.
+         */
+        [[nodiscard]] sf::Vector3f getTotalVelocity() const;
+
+        sf::Vector3f gravity_acceleration = {0, 0, 0};
+
+        /**
+         * @brief Prints the 3 velocities of the object with the given name in the format:
+         * \code
+         * name intrinsic_velocity: x = intrinsic_velocity.x, y = intrinsic_velocity.y, z = intrinsic_velocity.z
+         * ...
+         * \endcode
+         * @param name The name that would appear on the print function
+         */
+        void printVelocity(const std::string& name) const;
+
+        /**
+         * @brief Prints the 3 velocities of the object with the address of the object as its name.
+         */
+        void printVelocity() const;
+
+    private:
+        /**
+         * @return The address of the object in the form of a string.
+         */
+        std::string getAddressAsString() const;
+
+        /**
+         * @brief Velocity due to gravity. Is capped at \code MAX_FALL\endcode.
+         */
+        sf::Vector3f gravity_velocity = {0, 0, 0};
+    };
+}
+
+
+
+#endif //PHYSICSOBJECT_H
